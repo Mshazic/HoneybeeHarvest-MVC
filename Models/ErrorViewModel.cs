824fb42735d62122ede1b2bc562e22e6e04f8431
senzonzuza <senzokaqhaka@gmail.com>
2026-09-21 02:16:56 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HoneybeeHarvest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
